23341618c6485d9a4b62c63370bef3d81138ac64
wds-dxh <wdsnpshy@163.com>
2024-06-06 15:07:11 +0800

    前端框架基本完成，就是有点丑

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+//导入element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+//注册所有图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+app.use(ElementPlus).mount('#app')      //要最后挂载，否则会出现样式覆盖问题
