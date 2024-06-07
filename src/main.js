455a84166554d8e979bf539236a64e1d5b2751d6
wds-dxh <wdsnpshy@163.com>
2024-06-07 08:00:46 +0800

    完成了登录界面！！

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 //导入element-plus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
+import router from './router'
 //注册所有图标
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
@@ -13,4 +13,5 @@ const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
+app.use(router)
 app.use(ElementPlus).mount('#app')      //要最后挂载，否则会出现样式覆盖问题
